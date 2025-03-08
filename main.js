f646e1a779b08902e308871c9418dd9081009aff
RQuimera <quimeracomics3@gmail.com>
2025-03-07 23:20:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-
 document.querySelector(".menu-btn").addEventListener("click", () => {
   document.querySelector(".nav-menu").classList.toggle("show");
 })
 ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
-// ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 
 // Import the Firebase libraries
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
@@ -34,14 +32,15 @@ const params = new URLSearchParams(window.location.search);
 const chapterId = params.get("id");
 
 let resultContainer;
-
-if (chapterId) {
+if (path.includes("itsatrap.html")) {
+  resultContainer = document.querySelector('.cuartoSecreto');
+} else if (chapterId) {
   resultContainer = document.querySelector('.chapter-detail');
 } else if (path.includes("index.html") || path === "/") {
   resultContainer = document.querySelector('.capitulos');
 } else if (path.includes("capitulos.html")) {
   resultContainer = document.querySelector('.allChapter');
-}
+} 
 
 // Variables globales para paginación (usadas en "capitulos.html" sin parámetro "id")
 let allEpisodes = [];
@@ -50,13 +49,18 @@ const pageSize = 5;
 let prevButton, nextButton;
 
 if (chapterId) {
-  getIdEpisode().then((capitulo) => {
-    if (capitulo) {
-      displayIdEpisode(capitulo);
-    } else {
-      resultContainer.innerHTML = `<p>No se encontró el episodio con id ${chapterId}</p>`;
-    }
-  });
+  if (path.includes("itsatrap.html")) {
+    displaySecrets()
+  }else {
+    getIdEpisode().then((capitulo) => {
+      if (capitulo) {
+        displayIdEpisode(capitulo);
+      } else {
+        resultContainer.innerHTML = `<p>No se encontró el episodio con id ${chapterId}</p>`;
+      }
+    });
+  }
+  
 } else {
   getPodcastEpisodes().then((episodios) => {
     if (path.includes("index.html") || path === "/") {
@@ -117,71 +121,6 @@ async function getIdEpisode() {
   }
 }
 
-// function displayEpisodes(episodios) {
-//   resultContainer.innerHTML = '';
-
-//   if (episodios.length === 0) {
-//     let noEpisodeMessage = document.createElement('h4');
-//     resultContainer.classList.replace('capitulos', 'noCapt')
-//     noEpisodeMessage.textContent = 'Lo sentimos, nuestros capitulos fueron borrados por un problema en el multiverso, estamos buscando las esferas del dragon para recuperarlos';
-//     resultContainer.appendChild(noEpisodeMessage);
-//     return;
-//   }
-
-//   episodios.forEach(capitulo => {
-//     let captDiv = document.createElement('div');
-//     captDiv.classList.add('capt');
-
-//     const maxPalabras = 20;
-//     const palabras = capitulo.description.split(/\s+/); // Split por espacios
-
-//     if (path.includes("index.html") || path === "/") {
-//       let poster = document.createElement('img');
-//       poster.src = `https://drive.google.com/thumbnail?id=${capitulo.img}`;
-//       poster.alt = `Póster de ${capitulo.nombre}`;
-
-//       let title = document.createElement('h3');
-//       title.textContent = `Ep. #${capitulo.id} / ${capitulo.nombre}`;
-
-//       let description = document.createElement('p');
-//       if (palabras.length > maxPalabras) {
-//         description.textContent = palabras.slice(0, maxPalabras).join(' ') + ' ...';
-//       } else {
-//         description.textContent = capitulo.description;
-//       }
-
-//       let botonCapt = document.createElement('a');
-//       botonCapt.href = `capitulos.html?id=${capitulo.id}`;
-//       botonCapt.innerHTML = 'Ver más <i class="fas fa-angle-double-right"></i>';
-
-//       captDiv.append(poster, title, description, botonCapt);
-//       resultContainer.appendChild(captDiv);
-//     }
-//     else if (path.includes("capitulos.html")) {
-//       let infoDiv = document.createElement('div');
-//       infoDiv.classList.add('info');
-
-//       let poster = document.createElement('img');
-//       poster.src = `https://drive.google.com/thumbnail?id=${capitulo.img}`;
-//       poster.alt = `Póster de ${capitulo.nombre}`;
-
-//       let title = document.createElement('h3');
-//       title.textContent = `Ep. #${capitulo.id} / ${capitulo.nombre}`;
-
-//       let description = document.createElement('p');
-//       description.textContent = capitulo.description;
-
-//       let botonCapt = document.createElement('a');
-//       botonCapt.href = capitulo.link;
-//       // botonCapt.href = `capitulos.html?id=${capitulo.id}`;
-//       botonCapt.innerHTML = 'Escucha el capitulo  <i class="fas fa-angle-double-right"></i>';
-
-//       infoDiv.append(poster,title, description, botonCapt);
-//       captDiv.append(poster, infoDiv);
-//       resultContainer.appendChild(captDiv);
-//     }
-//   });
-// }
 function displayEpisodes(episodios) {
   resultContainer.innerHTML = '';
 
@@ -201,7 +140,8 @@ function displayEpisodes(episodios) {
     const palabras = capitulo.description.split(/\s+/); // Split por espacios
 
     let poster = document.createElement('img');
-    poster.src = `https://drive.google.com/thumbnail?id=${capitulo.img}`;
+    poster.src = capitulo.img;
+    // poster.src = `https://drive.google.com/thumbnail?id=${capitulo.img}`;
     poster.alt = `Póster de ${capitulo.nombre}`;
 
     let title = document.createElement('h3');
@@ -230,14 +170,13 @@ function displayIdEpisode(capitulo) {
   captDiv.classList.add('capts');
 
   let poster = document.createElement('img');
-  poster.src = `https://drive.google.com/thumbnail?id=${capitulo.img}`;
+  poster.src = capitulo.img;
+  // poster.src = `https://drive.google.com/thumbnail?id=${capitulo.img}`;
   poster.alt = `Póster de ${capitulo.nombre}`;
 
   let title = document.createElement('h3');
   title.textContent = `Ep. #${capitulo.id}/ ${capitulo.nombre}`;
 
-  // const maxPalabras = 20;
-  // const palabras = capitulo.description.split(/\s+/); // Split por espacios
   let description = document.createElement('p');
   description.textContent = capitulo.description;
 
@@ -283,7 +222,8 @@ function displayEpisodesPage(page) {
     infoDiv.classList.add('info');
 
     let poster = document.createElement('img');
-    poster.src = `https://drive.google.com/thumbnail?id=${capitulo.img}`;
+    poster.src = capitulo.img;
+    // poster.src = `https://drive.google.com/thumbnail?id=${capitulo.img}`;
     poster.alt = `Póster de ${capitulo.nombre}`;
 
     let title = document.createElement('h3');
@@ -303,49 +243,7 @@ function displayEpisodesPage(page) {
     resultContainer.appendChild(captDiv);
   });
 }
-// // Función para crear y actualizar controles de paginación en capitulos.html
-// function createPaginationControls() {
-//   let paginationDiv = document.createElement('div');
-//   paginationDiv.id = "pagination";
-//   paginationDiv.style.textAlign = "center";
-//   paginationDiv.style.marginTop = "20px";
 
-//   let prevButton = document.createElement('button');
-//   prevButton.textContent = "Anterior";
-//   prevButton.addEventListener('click', () => {
-//     if (currentPage > 1) {
-//       currentPage--;
-//       displayEpisodesPage(currentPage);
-//       updatePaginationControls();
-//     }
-//   });
-
-//   let nextButton = document.createElement('button');
-//   nextButton.textContent = "Siguiente";
-//   nextButton.addEventListener('click', () => {
-//     if (currentPage < Math.ceil(allEpisodes.length / pageSize)) {
-//       currentPage++;
-//       displayEpisodesPage(currentPage);
-//       updatePaginationControls();
-//     }
-//   });
-
-//   let pageInfo = document.createElement('span');
-//   pageInfo.id = "pageInfo";
-//   pageInfo.style.marginLeft = "10px";
-//   pageInfo.textContent = ` Página ${currentPage} de ${Math.ceil(allEpisodes.length / pageSize)}`;
-
-//   paginationDiv.append(prevButton, nextButton, pageInfo);
-//   // Insertar el contenedor de paginación justo después del resultContainer
-//   resultContainer.parentNode.insertBefore(paginationDiv, resultContainer.nextSibling);
-// }
-
-// function updatePaginationControls() {
-//   let pageInfo = document.getElementById('pageInfo');
-//   if (pageInfo) {
-//     pageInfo.textContent = ` Página ${currentPage} de ${Math.ceil(allEpisodes.length / pageSize)}`;
-//   }
-// }
 function createPaginationControls() {
   let paginationDiv = document.createElement('div');
   paginationDiv.id = "pagination";
@@ -405,4 +303,36 @@ function updatePaginationControls() {
   } else {
     nextButton.style.display = "inline-block";
   }
+}
+
+
+
+function displaySecrets() {
+
+  resultContainer.innerHTML = '';
+  let secretDiv = document.createElement('div');
+  secretDiv.classList.add('itsASecret');
+
+  let title = document.createElement('h3');
+  title.classList.add('secretText');
+  let poster = document.createElement('img');
+  poster.classList.add('secretImg');
+
+  if (chapterId  === 'c1') {
+    title.textContent = `Yohohohohohou, vienes a tomar una taza de té con nosotros?`;
+    poster.src = 'https://64.media.tumblr.com/0619e457fd122e3033e1887d23e9e695/404b197327442998-f9/s1280x1920/f9f8dd9e31f9cbe980aa48c8527bddd47e01312d.jpg';
+    poster.alt = `Gato con botas`;
+  } else if (chapterId === 'j1') {
+    title.textContent = `que haces aquí? Es peligroso que vayas solo por ahi, toma este kuriboh para que te acompañe`;
+    poster.src = 'https://live.staticflickr.com/65535/54371249032_3943105ff3_b.jpg';
+    poster.alt = `Kuriboh`;
+  } else if (chapterId === 'p1') {
+    title.textContent = `Zoro? como llegaste aquí? te perdiste de nuevo?`;
+    poster.src = 'https://i.blogs.es/54f024/zoro/1366_2000.jpeg';
+    poster.alt = `zoro?`;
+  } 
+
+  secretDiv.appendChild(poster);
+  secretDiv.appendChild(title);
+  resultContainer.appendChild(secretDiv);
 }
